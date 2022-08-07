0a4393e207d7019ce7720fa0537fde2f6f6a87bb
Hassam-check <hussyahmed6@gmail.com>
2022-08-07 22:19:05 +0200

    [04 commit] hello basit sexy

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("hassam");
         System.out.println("hassam malik");
         System.out.println("hassam AHMED");
+        System.out.println("malik");
+        System.out.println("AHMED");
     }
 }
